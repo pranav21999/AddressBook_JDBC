28f6d32812871df33a6436d3b4e4fcdd94a29c19
pranav21999 <pranavdani99@gmail.com>
2021-08-02 00:16:44 +0530

    Added:Retrive contact of database that were addrd in particular period

--- src/main/java/com/db/AddressBookDBService.java
+++ src/main/java/com/db/AddressBookDBService.java
@@ -1,109 +1,134 @@
 package com.db;
-
 import java.sql.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-/*
- * purpose:Retirves all entries form database to java_File
- * */
 public class AddressBookDBService {
 
-	private static AddressBookDBService addressBookDBService;
+    private static AddressBookDBService addressBookDBService;
+    private PreparedStatement addressBookDataStatement;
 
-	public static AddressBookDBService getInstance() {
-		if (addressBookDBService == null)
-			addressBookDBService = new AddressBookDBService();
-		return addressBookDBService;
-	}
-
-	private AddressBookDBService() {
+    private AddressBookDBService() {
 
     }
 
-	/*
-	 * purpose: connect JDBC connection used:Driver manager class
-	 */
-	private Connection getConnection() throws SQLException {
-		String jdbcURL = "jdbc:mysql://localhost:3306/addressbook_db";
-		String userName = "root";
-		String password = "root1999";
-		Connection connection;
-		System.out.println("Connecting to database:" + jdbcURL);
-		connection = DriverManager.getConnection(jdbcURL, userName, password);
-		System.out.println("Connection is successful!!!" + connection);
-		return connection;
-	}
+    public static AddressBookDBService getInstance() {
+        if (addressBookDBService == null)
+            addressBookDBService = new AddressBookDBService();
+        return addressBookDBService;
+    }
 
-	/*
-	 * read data from database
-	 */
-	public List<Person> readData() {
-		String query = "SELECT * from address_book;";
-		return this.getPersonDetailsFromDatabase(query);
-	}
+    private Connection getConnection() throws SQLException {
+        String jdbcURL = "jdbc:mysql://localhost:3306/addressbook_db?useSSL=false";
+        String userName = "root";
+        String password = "root1999";
+        Connection connection;
+        System.out.println("Connecting to database:" + jdbcURL);
+        connection = DriverManager.getConnection(jdbcURL, userName, password);
+        System.out.println("Connection is successful!!!" + connection);
+        return connection;
+    }
 
-	/*
-	 * getting PersonDetails form Pojo class Person
-	 */
-	private List<Person> getPersonDetailsFromDatabase(String query) {
-		List<Person> personList = new ArrayList<Person>();
-		try (Connection connection = this.getConnection()) {
-			Statement statement = connection.createStatement();
-			ResultSet resultSet = statement.executeQuery(query);
-			personList = this.getPersonData(resultSet);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return personList;
-	}
-	
-	/*
-	 * update details of address book by their name
-	 * */
-	 public int updateContactNumber(String firstName, String contactNumber) {
-	        return this.updateAddressBookDataUsingStatement(firstName, contactNumber);
-	    }
 
-		/*
-		 * update details of address book by their name
-		 * update phoneNo of name "pranav"
-		 * */
+    public List<Person> readData() {
+        String query = "SELECT * from address_book;";
+        return this.getPersonDetailsFromDatabase(query);
+    }
 
-	    private int updateAddressBookDataUsingStatement(String firstName, String mobileNumber) {
-	        String sql = String.format("update address_book set mobileNumber = '%s' where firstName = '%s';", mobileNumber, firstName);
-	        try (Connection connection = this.getConnection()) {
-	            Statement statement = connection.createStatement();
-	            return statement.executeUpdate(sql);
-	        } catch (SQLException e) {
-	            e.printStackTrace();
-	        }
-	        return 0;
-	    }
+    private List<Person> getPersonDetailsFromDatabase(String query) {
+        List<Person> personList = new ArrayList<Person>();
+        try (Connection connection = this.getConnection()) {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+            personList = this.getPersonData(resultSet);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return personList;
+    }
 
-	private List<Person> getPersonData(ResultSet resultSet) {
+    private List<Person> getPersonData(ResultSet resultSet) {
 
-		List<Person> personList = new ArrayList<>();
-		try {
-			while (resultSet.next()) {
-				int id = resultSet.getInt("id");
-				String firstName = resultSet.getString("firstName");
-				String lastName = resultSet.getString("lastName");
-				String address = resultSet.getString("address");
-				String city = resultSet.getString("city");
-				String state = resultSet.getString("state");
-				String zip = resultSet.getString("zip");
-				String mobileNumber = resultSet.getString("mobileNumber");
-				String email = resultSet.getString("email");
-				personList.add(new Person(id, firstName, lastName, address, city, state, zip, mobileNumber, email));
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return personList;
+        List<Person> personList = new ArrayList<>();
+        try {
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String firstName = resultSet.getString("firstName");
+                String lastName = resultSet.getString("lastName");
+                String address = resultSet.getString("address");
+                String city = resultSet.getString("city");
+                String state = resultSet.getString("state");
+                String zip = resultSet.getString("zip");
+                String mobileNumber = resultSet.getString("mobileNumber");
+                String email = resultSet.getString("email");
+                LocalDate entryDate = resultSet.getDate("entryDate").toLocalDate();
+                personList.add(new Person(id,firstName,lastName,address,city,state,zip,mobileNumber,email,entryDate));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return personList;
 
-	}
+    }
+
+    public int updateContactNumber(String firstName, String contactNumber) {
+        return this.updateAddressBookDataUsingStatement(firstName, contactNumber);
+
+    }
+
+    private int updateAddressBookDataUsingStatement(String firstName, String mobileNumber) {
+        String sql = String.format("update address_book set mobileNumber = '%s' where firstName = '%s';", mobileNumber, firstName);
+        try (Connection connection = this.getConnection()) {
+            Statement statement = connection.createStatement();
+            return statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public List<Person> getaddressBookData(String firstName) {
+        List<Person> personList = null;
+        if (this.addressBookDataStatement == null)
+            this.prepareStatementForAddressBookData();
+        try {
+            addressBookDataStatement.setString(1, firstName);
+            ResultSet resultSet = addressBookDataStatement.executeQuery();
+            personList = this.getPersonData(resultSet);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return personList;
+    }
+
+    private void prepareStatementForAddressBookData() {
+        try {
+            Connection connection = this.getConnection();
+            String sql = "SELECT * FROM addressbook WHERE firstName = ?";
+            addressBookDataStatement = connection.prepareStatement(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<Person> getAddressBookForDateRange(LocalDate startDate, LocalDate endDate) {
+        String sql = String.format("SELECT * FROM address_book WHERE entryDate BETWEEN '%s' AND '%s';",
+                Date.valueOf(startDate), Date.valueOf(endDate));
+        return this.getAddressBookDataUsingDB(sql);
+    }
+
+    private List<Person> getAddressBookDataUsingDB(String sql) {
+        List<Person> addressBookList = new ArrayList<>();
+        try (Connection connection = this.getConnection()) {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+            addressBookList = this.getPersonData(resultSet);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return addressBookList;
+    }
 
 	//main
 	public static void main(String[] args) {
@@ -112,5 +137,6 @@ public class AddressBookDBService {
 		List<Person> dataList = addressBookDBService.readData();
 		addressBookDBService.updateAddressBookDataUsingStatement("abc", "9999999999");
 		System.out.println(dataList);
+		
 	}
 }
